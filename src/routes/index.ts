9cc75ddab9e7e6fa6fbd07d1c239acd64538a8be
sanchitjainthb <sanchit@thb.co.in>
2019-11-04 13:38:57 +0530

    first commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,6 @@ import express = require('express');
 let router = (express as any).Router();
 
 const asyncMiddleware = fn => (req, res, next) => {
-    var Promise = require('es6-promise').Promise;
     Promise.resolve(fn(req, res, next))
         .catch(next);
 };
